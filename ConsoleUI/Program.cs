82367ffe121dafd14327e68b63409a843ec1e3f3
MERT RAMAZANOĞLU <mert.ramazanoglu@hotmail.com>
2023-02-16 19:01:04 +0300

    result ve message

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -41,21 +41,25 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             //ProductManager productManager = new ProductManager(new EfProductDal()
-            //    , new CategoryManager(new EfCategoryDal()));
+            //    , new CategoryManager(new EfCategoryDal
+            //    
 
-            //var result = productManager.GetProductDetails();
+            ProductManager productManager = new ProductManager(new EfProductDal());
+                
 
-            //if (result.Success == true)
-            //{
-            //    foreach (var product in result.Data)
-            //    {
-            //        Console.WriteLine(product.ProductName + "/" + product.CategoryName);
-            //    }
-            //}
-            //else
-            //{
-            //    Console.WriteLine(result.Message);
-            //}
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
 
 
         }
